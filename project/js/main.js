372a6f54c414104defab50fec947696cfd8de3fb
Cat <zhangycathleen@gmail.com>
2019-03-30 14:32:00 -0400

    added firebase from google

--- project/js/main.js
+++ project/js/main.js
@@ -9,3 +9,20 @@ function myFunction() {
   $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
 }
 
+// gets called when the user clicks this
+function toggleSignIn() {
+    if (!firebase.auth().currentUser) { // if the user's not logged in, handle login
+        var provider = new firebase.auth.GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/plus.login');
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+                                                       console.log("success");
+                                                       }).catch(function(error) {
+                                                                console.error("error", error);
+                                                                });
+    } else { // handle logout
+        firebase.auth().signOut();
+    }
+        //This disables the button until login or logout is successful. You'll want to replace 'login-button' with the id of your login button.
+    $('signin').attr("disabled", true);
+}
+}
